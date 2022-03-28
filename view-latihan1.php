7f27fd73994ca9022d9aeb19c882a2664d3c12fd
fhendrawan12 <101867484+fhendrawan12@users.noreply.github.com>
2022-03-28 21:42:05 +0700

    Add files via upload

---
+++ view-latihan1.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+ <title>Latihan 1</title>
+</head>
+<body>
+saya fajar,sedang belajar web programming 2<br>
+ Nilai 1 = <?= $nilai1; ?>
+ Nilai 2 = <?= $nilai2; ?>
+ ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
